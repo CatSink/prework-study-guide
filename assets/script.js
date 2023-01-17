7eba5c6ebcc017f097a808ad09ca4aca7f8022b7
CatSink <121461783+CatSink@users.noreply.github.com>
2023-01-16 19:28:18 -0500

    Update script.js
    removed extra lines of listTopics, and selectTopic

--- assets/script.js
+++ assets/script.js
@@ -24,8 +24,6 @@ if (randomTopic === 'HTML') {
   console.log('Please try again!');
     }
 }
-listTopics()
-selectTopic()
 console.log('Here are the topics we learned through Prework:');
 listTopics();
 console.log('Which topic should we study first?');
